8ad5b09f72dcccd76f21c846df2b037a7a45850a
Luan santos <luansantosdev.py@gmail.com>
2023-09-10 11:01:12 -0300

    feat: create new route and add in v1

---
+++ src/api/routes/v1/auth.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { container } from 'tsyringe';
+import * as validations from '../../../application/validations/authControllerRequestValidations';
+import AuthController from '../../controllers/authController';
+
+export default async (): Promise<Router> => {
+  const router = Router();
+  const authController = container.resolve(AuthController);
+
+  router.post('/register',validations.registerUserRequestValidation,
+  authController.register);
+
+  return router;
+};
