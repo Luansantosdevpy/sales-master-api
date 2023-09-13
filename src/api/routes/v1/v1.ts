1351adc366679a54fd8312e4e579d5f81f3cc4ca
Luan santos <luansantosdev.py@gmail.com>
2023-09-13 17:50:45 -0300

    feat: create category router

--- src/api/routes/v1/v1.ts
+++ src/api/routes/v1/v1.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import healthcheck from './healthcheck';
 import auth from './auth';
 import client from './client';
+import category from './category';
 
 export default async (): Promise<Router> => {
   const router = Router();
@@ -10,6 +11,7 @@ export default async (): Promise<Router> => {
   router.use('/health-check', await healthcheck());
   router.use('/user', await auth());
   router.use('/client', await client());
+  router.use('/category', await category());
 
   return router;
 };
