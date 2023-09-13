7181fad5e2d120517faa85770c7102f2bcdfcdc6
Luan santos <luansantosdev.py@gmail.com>
2023-09-13 09:00:58 -0300

    feat: migration to create product table and category table

---
+++ src/infrastructure/data/migrations/20230913114316-create_table_products.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => await queryInterface.createTable('products', {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    price: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    product_stock: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    sku: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    category: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      references: {
+        model: 'categories',
+        key: 'id'
+      }
+    },
+    provider: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    attributes: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('now()')
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('now()')
+    }
+  }),
+  down: async (queryInterface, Sequelize) => await queryInterface.dropTable('products')
+};
