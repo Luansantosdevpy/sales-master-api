86f6167037916879aff220d64c8727d9a7a7afe3
Luan santos <luansantosdev.py@gmail.com>
2023-09-15 17:52:51 -0300

    feat: migration to create table sales

---
+++ src/infrastructure/data/migrations/20230915202343-create_table_sales.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      client: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'clients',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      total: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      itensSale: {
+        type: Sequelize.JSONB,
+        defaultValue: []
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
