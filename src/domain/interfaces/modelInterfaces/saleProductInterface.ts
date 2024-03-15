a7adcfdc1b3786b259b801e52e44ed14f9a8457e
luansantosdevpy <luansantosdev.py@gmail.com>
2024-03-15 18:02:55 -0300

    feat: remove interface to product

--- src/domain/interfaces/modelInterfaces/saleProductInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export default interface ISaleProducts extends Document {
-    saleId: string;
-    productId: string;
-    quantity: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
