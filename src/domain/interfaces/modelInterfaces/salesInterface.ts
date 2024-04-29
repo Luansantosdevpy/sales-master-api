f415d6bd25046d29556573c9f40586d6bf558830
ThSoaresDev <ThiagoSoaresVasconcelos@outlook.com>
2024-04-29 14:32:17 -0300

    commit temporário

--- src/domain/interfaces/modelInterfaces/salesInterface.ts
+++ src/domain/interfaces/modelInterfaces/salesInterface.ts
@@ -1,10 +1,17 @@
-import { Document } from "mongoose";
-
+import { Document } from 'mongoose';
 export default interface ISale extends Document {
-    date: Date;
-    clientId: string;
-    total: number;
-    itensSale: string[];
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
+  date: Date;
+  clientId: string;
+  total: number;
+  itensSale: string[];
+  createdAt?: Date;
+  updatedAt?: Date;
+  typeOfSale: {
+    type: String;
+    enum: ['pickupOrder', 'inStoreOrder', 'tableOrder'];
+  };
+  timeOfSale: {
+    type: String;
+    enum: ['wait', 'prepare', 'done'];
+  };
+}
